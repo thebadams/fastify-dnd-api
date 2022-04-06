d52f9d136f44542961a03a9e894729949b5c3b56
Brian P. Adams <bpeteradams@gmail.com>
2022-04-06 09:06:52 -0400

    Spells(data): Added cantrip data and adjusted spells service to read file

--- src/spells/spells.service.ts
+++ src/spells/spells.service.ts
@@ -1,5 +1,12 @@
+import { parse } from 'csv';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 export const findAllSpells = async () => {
-  return 'All Spells';
+  const PATH = join(__dirname, '../../data/spells/cantrips.json');
+  const file = await readFile(PATH, 'utf-8');
+  console.log(file);
+  const json = await JSON.parse(file);
+  return json;
 };
 
 export const findSpellByName = async (name: string) => {
