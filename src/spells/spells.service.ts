75b6e04695294a1ee53795d10f152b01bdaa60ca
Brian P. Adams <bpeteradams@gmail.com>
2022-04-06 09:42:59 -0400

    Spells(services): Refactored spells services to use spells.json (which holds all spell data; wrote a SpellDTO type

--- src/spells/spells.service.ts
+++ src/spells/spells.service.ts
@@ -1,14 +1,33 @@
-import { parse } from 'csv';
-import { readFile } from 'fs/promises';
-import { join } from 'path';
+import readJSON, { FILE_NAMES } from '../utils/readJSON';
+
+type SpellDTO = {
+  Name: string;
+  Source: string;
+  Level: string;
+  'Casting Time': string;
+  Duration: string;
+  School: string;
+  Range: string;
+  Components: string;
+  Classes: string;
+  Text: string;
+  'At Higher Levels': string;
+};
+
 export const findAllSpells = async () => {
-  const PATH = join(__dirname, '../../data/spells/cantrips.json');
-  const file = await readFile(PATH, 'utf-8');
-  console.log(file);
+  const file = await readJSON(FILE_NAMES.SPELLS);
+  // console.log(file);
   const json = await JSON.parse(file);
   return json;
 };
 
 export const findSpellByName = async (name: string) => {
-  return `Spell Name: ${name}`;
+  const file = await readJSON(FILE_NAMES.SPELLS);
+  const json = (await JSON.parse(file)) as SpellDTO[];
+  const spell = json.find((spell) => spell.Name === name);
+  if (spell) {
+    return spell;
+  } else {
+    return 'No Spell With That Name Found';
+  }
 };
