10a92a1d3c56f099b1dd9ce134b6b9da94413610
Brian P. Adams <bpeteradams@gmail.com>
2022-04-05 23:20:11 -0400

    Server(main): edited the main start up bootstrapping method to exit with code 1 if there is an error on listen; added typeing to PORT variable

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const main = async () => {
   server = await registerServer();
   console.log(process.env.PORT);
   try {
-    let PORT;
+    let PORT: string;
     if (process.env.PORT) {
       PORT = process.env.PORT;
     } else {
@@ -17,6 +17,7 @@ const main = async () => {
     server.log.info(`Server has started on Port: ${PORT}`);
   } catch (error) {
     server.log.error('There was an error starting the server', error);
+    process.exit(1);
   }
 };
 
