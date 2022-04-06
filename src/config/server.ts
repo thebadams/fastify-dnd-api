3c7823d113489d9a3b70e42dc1186cebf4066e7c
Brian P. Adams <bpeteradams@gmail.com>
2022-04-05 20:39:05 -0400

    Server(HelloWorld): Added a hello world response to the server at /hello

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,4 +2,8 @@ import fastify from 'fastify';
 
 const server = fastify({ logger: true });
 export const PORT = 3001;
+
+server.get('/hello', async (request, reply) => {
+  return { message: 'Hello World' };
+});
 export default server;
