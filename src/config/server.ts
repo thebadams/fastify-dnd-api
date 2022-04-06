4864f9a54caed7dbfcd1aa92697291657f1925b2
Brian P. Adams <bpeteradams@gmail.com>
2022-04-05 23:04:23 -0400

    Env(configuration): Configured .env using fastify-env plugin

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,12 +1,49 @@
 import fastify from 'fastify';
+import fastifyEnv, { fastifyEnvOpt } from 'fastify-env';
 import { registerSpellRoutes } from '../spells/spells.route';
 
-const server = fastify({ logger: true });
-export const PORT = 3001;
+declare module 'fastify' {
+  interface FastifyInstance {
+    config: {
+      PORT: string;
+    };
+  }
+}
 
-server.get('/hello', async (request, reply) => {
-  return { message: 'Hello World' };
-});
+const schema = {
+  type: 'object',
+  required: ['PORT'],
+  properties: {
+    PORT: {
+      type: 'string',
+      default: 3001,
+    },
+  },
+};
 
-server.register(registerSpellRoutes, { prefix: '/api' });
-export default server;
+const options: fastifyEnvOpt = {
+  confKey: 'config',
+  schema,
+  data: process.env,
+  dotenv: true,
+};
+const registerServer = async () => {
+  const server = fastify({ logger: true });
+  server.register(fastifyEnv, options).ready((error) => {
+    if (error) {
+      console.error(error);
+    }
+
+    console.log(server.config);
+  });
+
+  server.get('/hello', async (request, reply) => {
+    return { message: 'Hello World' };
+  });
+
+  server.register(registerSpellRoutes, { prefix: '/api' });
+  await server.after();
+  return server;
+};
+
+export default registerServer;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import { FastifyInstance } from 'fastify';
-import server, { PORT } from './config/server';
+import registerServer from './config/server';
 
-const main = async (server: FastifyInstance, port: number) => {
+const main = async () => {
+  let server: FastifyInstance;
+
+  server = await registerServer();
+  console.log(process.env.PORT);
   try {
-    await server.listen(port);
-    server.log.info(`Server has started on Port: ${port}`);
+    let PORT;
+    if (process.env.PORT) {
+      PORT = process.env.PORT;
+    } else {
+      PORT = server.config.PORT;
+    }
+    await server.listen(PORT);
+    server.log.info(`Server has started on Port: ${PORT}`);
   } catch (error) {
     server.log.error('There was an error starting the server', error);
   }
 };
 
-main(server, PORT);
+main();
